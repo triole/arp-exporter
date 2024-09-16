8c7c83a1d1ce498c8af8d0828c2ab747360c0302
olaf michaelis <o.mic@web.de>
2024-09-16 20:29:13 +0200

    Add name and interface to prometheus metrics

--- src/ae/encode.go
+++ src/ae/encode.go
@@ -6,9 +6,16 @@ func (ae *tAE) makePrometheusMetrics() (metrics string) {
 		metrics += "#TYPE\n"
 		metrics += "arp_exporter{"
 		metrics += "ip=\"" + el.IP + "\", mac=\"" + el.MAC
+		if el.Itf != "" {
+			metrics += ", itf=\"" + el.Itf
+		}
+		if el.Name != "" {
+			metrics += ", name=\"" + el.Name
+		}
 		if el.Vendor != "" {
 			metrics += ", vendor=\"" + el.Vendor
 		}
+
 		metrics += "\"} 0\n"
 	}
 	return
